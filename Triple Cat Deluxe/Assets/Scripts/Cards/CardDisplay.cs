bf35f524c9ffe2b6bec3228c655535fac1cef630
Hedgy134117 <hedgy134117@gmail.com>
2019-03-28 07:42:15 -0400

    Update CardDisplay.cs

--- Triple Cat Deluxe/Assets/Scripts/Cards/CardDisplay.cs
+++ Triple Cat Deluxe/Assets/Scripts/Cards/CardDisplay.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+// allows it so the cards can be displayed in the game
+// (allows a gameobject to have a card attached to it)
 public class CardDisplay : MonoBehaviour {
 
     public Card card;
